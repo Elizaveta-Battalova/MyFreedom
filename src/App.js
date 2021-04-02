7c30a7ba3ab4086b6fe78cfcdc1bd164bf7ad2e7
Elizaveta-Battalova <73611395+Elizaveta-Battalova@users.noreply.github.com>
2021-04-02 22:07:55 +0300

    Add files via upload
    ADDED MY APP

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Article from "./components/Article";
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Article/>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/Article/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./styles.css";
+
+
+class Article extends React.Component {
+    render() {
+      return (
+        <div className="Article">
+        <h1> Hello, world! </h1>
+        <p>some text</p>
+        </div>
+      );
+    }
+  }
+  
+  export default Article;
+
+
