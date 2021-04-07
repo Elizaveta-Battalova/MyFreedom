feaaff5e20978d138d59b491bab579571ee588c4
Elizaveta-Battalova <73611395+Elizaveta-Battalova@users.noreply.github.com>
2021-04-07 18:15:40 +0300

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return(
+        <div>
+             <body>
+        <div class="wrapper">
+            <header>
+                <div class="container flex-wrapper">
+                    <a class="logo" href="#"></a>
+                    <div class="toggle">
+                        |||
+                    </div>
+                    <nav>
+                        <a href="#">link1</a>
+                        <a href="#">link2</a>
+                        <a href="#">link3</a>
+                        <a href="#">link4</a>
+                        <a href="#">link5</a>
+                    </nav>
+                </div>
+            </header>
+            <main>
+                <div class="container">
+                    <h1>Heading</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eveniet? Eum deleniti officia
+                        eveniet
+                        quo ducimus. Non alias id libero doloribus tenetur ratione laboriosam, dicta in consequuntur cum
+                        placeat
+                        delectus.</p>
+                    <h2>Heading</h2>
+                    <div class="flex-wrapper">
+                        <div>
+                            <h3>Heading</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nihil odio inventore
+                                exercitationem ullam id. Perferendis magni illum amet velit?</p>
+                        </div>
+                        <div>
+                            <h3>Heading</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nihil odio inventore
+                                exercitationem ullam id. Perferendis magni illum amet velit?</p>
+                        </div>
+                        <div>
+                            <h3>Heading</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nihil odio inventore
+                                exercitationem ullam id. Perferendis magni illum amet velit?</p>
+                        </div>
+                        <div>
+                            <h3>Heading</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nihil odio inventore
+                                exercitationem ullam id. Perferendis magni illum amet velit?</p>
+                        </div>
+                        <div>
+                            <h3>Heading</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nihil odio inventore
+                                exercitationem ullam id. Perferendis magni illum amet velit?</p>
+                        </div>
+                        <div>
+                            <h3>Heading</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nihil odio inventore
+                                exercitationem ullam id. Perferendis magni illum amet velit?</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer>
+                <div class="container">
+                    <div>Lorem ipsum dolor sit amet.</div>
+                    <div>Lorem ipsum dolor, sit amet consectetur adipisicing.</div>
+                </div>
+            </footer>
+        </div>
+    </body>
+        </div>
+    );
+  }
+}
+
+export default App;
